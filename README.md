e5a09ecdc3d7526de447dbada1c3dfc305a35ec5
esoodev <esoodev@gmail.com>
2017-08-27 20:03:08 +0900

    POST routes work now,

--- README.md
+++ README.md
@@ -1,15 +1,39 @@
 # eve-swagger-simple
 Simple promise-based node.js module for utilizing Eve Swagger Interface.
+Please email me for bugs/suggestions!
 
 ##Usage
 ```
 var esis = require('eve-swagger-simple');
 
-async function main() {
-    var wallet = await esis.request('/characters/{character_id}/wallet/', { character_id: 0123, datasource: singularity, token: ...});
+// Prints to console the amount of ISK in the wallet.
+async function showWallet() {
+    var wallet = await esis.request('/characters/0123/wallet/', { 
+        character_id: 0123, 
+        datasource: singularity, 
+        token: (character's access token here)
+        // Add other parameters if you need.
+    });
     console.log(wallet);
 };
+showWallet();
 
-main();
-
+// Sets autopilot destination.
+async function setAutopilot() {
+    var autopilot = await esis.request('/ui/autopilot/waypoint/', {
+        datasource: 'tranquility',
+        add_to_beginning: true,
+        clear_other_waypoints: true,
+        destination_id: 30000142,
+        token: (character's access token here)
+        // Add other parameters if you need.
+    });
+    console.log(autopilot);
+};
+setAutopilot();
 ```
+
+
+##Update
+###1.0.2
+POST routes work now.  
--- index.js
+++ index.js
@@ -3,18 +3,42 @@ const baseUrl = ('https://esi.tech.ccp.is/latest');
 
 module.exports = {
 
-    // Send a request to ESI
+    // Send a GET request to ESI - try POST if it fails.
     request: function (route, parameters) {
 
         return new Promise((resolve, reject) => {
 
-            request({
+            request.get({
                 url: baseUrl+route,
                 qs: parameters
             }, (error, response, body) => {
                 if (error) {
-                    console.log('error:', error);
-                    reject();
+                    console.log(error);
+                    reject(error);
+                } else if (response && body) {
+                    var result = JSON.parse(body);
+                    if (result.error = 'Method not allowed') {
+                        return this._post(route, parameters);
+                    } else {
+                        resolve(JSON.parse(body));
+                    }
+                }
+            });
+        });
+    },
+
+    // Send a post request to ESI
+    _post: function (route, parameters) {
+
+        return new Promise((resolve, reject) => {
+
+            request.post({
+                url: baseUrl+route,
+                qs: parameters
+            }, (error, response, body) => {
+                if (error) {
+                    console.log(error);
+                    reject(error);
                 } else if (response && body) {
                     resolve(JSON.parse(body));
                 }
